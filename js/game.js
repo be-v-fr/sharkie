f432b3c1b4945a029b3c3329dfd748ca1c3c6842
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-02-07 08:24:11 +0100

    implemented ingame settings

--- js/game.js
+++ js/game.js
@@ -32,7 +32,7 @@ function load() {
         if (isLoaded()) {
             clearInterval(waitUntilLoaded);
             world.start();
-            overlay.style.display = 'none';
+            renderIngameOverlay();
         }
     }, 10);
 }
@@ -50,8 +50,13 @@ function isLoaded() {
     return loadingCounter >= TOTAL_NR_OF_IMAGES;
 }
 
+function renderIngameOverlay() {
+    overlay.classList.toggle('overlayBg');
+    overlay.innerHTML = generateIngameSettings();
+}
+
 function showSettings() {
-    overlay.innerHTML = generateSettings();
+    overlay.innerHTML = generateMenuSettings();
     renderSettings();
     addReturnListener();
 }
@@ -73,6 +78,15 @@ function setSound(on) {
 function setMusic(on) {
     settings['music'] = on;
     if (on && settings['sound']) {
+        turnMusicOn();
+    } else {
+        turnMusicOff();
+    }
+}
+
+function setIngameMusic() {
+    if (settings['music']) {
+        settings['sound'] = true;
         music['main'].volume = 0.14;
         music['boss'].volume = 0.23;
     } else {
@@ -83,61 +97,6 @@ function setMusic(on) {
 
 function setHardMode(on) {
     settings['hardMode'] = on;
-}
-
-function generateStartscreen() {
-    return /* html */ `
-        <button class="menuBtn rotateLeft" onclick="start()" onmousedown="playMenuSound()">Start Game</button>
-        <button class="menuBtn rotateRight" onmousedown="playMenuSound()">Instructions</button>
-        <button class="menuBtn rotateRight" onclick="showSettings()" onmousedown="playMenuSound()">Settings</button>
-    `;
-}
-
-function generateLoadingscreen() {
-    return /* html */ `
-        <div id="loadingWrapper">
-            <div id="loadingBarBg">
-                <div id="loadingBar"></div>
-            </div>
-        </div>
-    `;
-}
-
-function generateInstructions() {
-    return /* html */ `
-
-    `;
-}
-
-function generateSettings() {
-    return /* html */ `
-        <div class="menuPageWrapper" onmouseup="event.stopPropagation()">
-            <button class="close" onclick="returnToMain()">X</button>
-            <table class="settings" onclick="updateSettings()">
-                <tr>
-                    <td><span>Sound</span></td>
-                    <td><button id="sound1" onclick="setSound(true)" onmousedown="playMenuSound()">on</button></td>
-                    <td><button id="sound0" onclick="setSound(false)" onmousedown="playMenuSound()">off</button></td>
-                </tr>
-                <tr>
-                    <td>
-                        <svg width="32" height="24" xmlns="http://www.w3.org/2000/svg">
-                            <line x1="4" y1="2" x2="4" y2="18" stroke="white" stroke-width="2"/>
-                            <line x1="3" y1="17" x2="30" y2="17" stroke="white" stroke-width="2"/>
-                        </svg>                
-                        <span>Music</span>
-                    </td>
-                    <td><button id="music1" onclick="setMusic(true)" onmousedown="playMenuSound()">on</button></td>
-                    <td><button id="music0" onclick="setMusic(false)" onmousedown="playMenuSound()">off</button></td>
-                </tr>
-                <tr>    
-                    <td><span>Difficulty</span></td>
-                    <td><button id="hardMode0" onclick="setHardMode(false)" onmousedown="playMenuSound()">normal</button></td>
-                    <td><button id="hardMode1" onclick="setHardMode(true)" onmousedown="playMenuSound()">hard</button></td>
-                </tr>
-            </table>
-        </div>
-    `;
 }
 
 function updateSettings() {
@@ -175,4 +134,97 @@ function styleSettingsBtns(key, btn0, btn1) {
         btn1.classList.add('settingsBtnInactive');
         btn0.classList.add('settingsBtnActive');
     }
+}
+
+function generateStartscreen() {
+    return /* html */ `
+        <button class="menuBtn rotateLeft" onclick="start()" onmousedown="playMenuSound()">Start Game</button>
+        <button class="menuBtn rotateRight" onmousedown="playMenuSound()">Instructions</button>
+        <button class="menuBtn rotateRight" onclick="showSettings()" onmousedown="playMenuSound()">Settings</button>
+    `;
+}
+
+function generateLoadingscreen() {
+    return /* html */ `
+        <div id="loadingWrapper">
+            <div id="loadingBarBg">
+                <div id="loadingBar"></div>
+            </div>
+        </div>
+    `;
+}
+
+function generateInstructions() {
+    return /* html */ `
+
+    `;
+}
+
+function generateMenuSettings() {
+    return /* html */ `
+        <div class="menuPageWrapper" onmouseup="event.stopPropagation()">
+            <button class="close" onclick="returnToMain()">X</button>
+            <table class="settings" onclick="updateSettings()">
+                <tr>
+                    <td><span>Sound</span></td>
+                    <td><button id="sound1" onclick="setSound(true)" onmousedown="playMenuSound()">on</button></td>
+                    <td><button id="sound0" onclick="setSound(false)" onmousedown="playMenuSound()">off</button></td>
+                </tr>
+                <tr>
+                    <td>
+                        <svg width="32" height="24" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="4" y1="2" x2="4" y2="18" stroke="white" stroke-width="2"/>
+                            <line x1="3" y1="17" x2="30" y2="17" stroke="white" stroke-width="2"/>
+                        </svg>                
+                        <span>Music</span>
+                    </td>
+                    <td><button id="music1" onclick="setMusic(true)" onmousedown="playMenuSound()">on</button></td>
+                    <td><button id="music0" onclick="setMusic(false)" onmousedown="playMenuSound()">off</button></td>
+                </tr>
+                <tr>    
+                    <td><span>Difficulty</span></td>
+                    <td><button id="hardMode0" onclick="setHardMode(false)" onmousedown="playMenuSound()">normal</button></td>
+                    <td><button id="hardMode1" onclick="setHardMode(true)" onmousedown="playMenuSound()">hard</button></td>
+                </tr>
+            </table>
+        </div>
+    `;
+}
+
+function generateIngameSettings() {
+    return /* html */ `
+        <div class="ingameSettingsWrapper">
+            <button onclick="toggleIngameSound()"><img id="ingameSoundBtnImg" src="../img/marks/icons/sound.svg"></button>
+            <button onclick="toggleIngameMusic()"><img id="ingameMusicBtnImg" src="../img/marks/icons/music.svg"></button>
+        </div>
+    `;
+}
+
+function renderIngameSettings() {
+    const soundImg = document.getElementById('ingameSoundBtnImg');
+    const musicImg = document.getElementById('ingameMusicBtnImg');
+    if (settings['sound']) {
+        soundImg.src = '../img/marks/icons/sound.svg';
+        if (settings['music']) {
+            musicImg.src = '../img/marks/icons/music.svg';
+        } else {
+            musicImg.src = '../img/marks/icons/music_disabled.svg';
+        }
+    } else {
+        soundImg.src = '../img/marks/icons/sound_disabled.svg';
+        musicImg.src = '../img/marks/icons/music_disabled.svg';
+    }
+}
+
+function toggleIngameSound() {
+    settings['sound'] = !settings['sound'];
+    settings['music'] = settings['sound'];
+    setIngameMusic();
+    renderIngameSettings();
+}
+
+function toggleIngameMusic() {
+    settings['music'] = !settings['music'];
+    setIngameMusic();
+    renderIngameSettings();
 }
